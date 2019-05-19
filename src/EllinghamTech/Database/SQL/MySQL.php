ba33a3417a652d929d8232b22243baccb35f8c27
Kieran Youngman <kieran@ellinghamtech.co.uk>
2019-05-19 03:05:09 +0100

    Added documentation, throws a ConnectFailed exception on failure to connect/open/create the database.

--- src/EllinghamTech/Database/SQL/MySQL.php
+++ src/EllinghamTech/Database/SQL/MySQL.php
@@ -1,6 +1,8 @@
 <?php
 namespace EllinghamTech\Database\SQL;
 
+use EllinghamTech\Exceptions\Data\ConnectFailed;
+
 class MySQL extends Wrapper
 {
 	public function __construct()
@@ -8,6 +10,24 @@ class MySQL extends Wrapper
 		$this->database_type = 'MySQL';
 	}
 
+	/**
+	 * Opens a database connection to MySQL or compatible database
+	 * servers such as MariaDB.
+	 *
+	 * Opens a database connection to MySQL, MariaDB or compatible
+	 * database servers with the given host, user, pass and databaseName.
+	 *
+	 * Also accepts additional PDO options as a fifth param.  No return
+	 * but throws a ConnectFailed exception on failure.
+	 *
+	 * @param string $host
+	 * @param string $user
+	 * @param string $pass
+	 * @param string $databaseName
+	 * @param array|null $pdoOptions
+	 *
+	 * @throws ConnectFailed If the database connection could not be opened
+	 */
 	public function connect(string $host, string $user, string $pass, string $databaseName, ?array $pdoOptions=null) : void
 	{
 		if($this->db_link != null) return;
@@ -21,7 +41,7 @@ class MySQL extends Wrapper
 		catch(\PDOException $e)
 		{
 			$this->db_link = null;
-			throw new \Exception("Failed to load database connection: ".$e->getMessage());
+			throw new ConnectFailed('Failed to load database connection: '.$e->getMessage(), 0, $e);
 		}
 	}
 }
--- src/EllinghamTech/Database/SQL/SQLite.php
+++ src/EllinghamTech/Database/SQL/SQLite.php
@@ -1,6 +1,8 @@
 <?php
 namespace EllinghamTech\Database\SQL;
 
+use EllinghamTech\Exceptions\Data\ConnectFailed;
+
 class SQLite extends Wrapper
 {
 	public function __construct()
@@ -9,11 +11,15 @@ class SQLite extends Wrapper
 	}
 
 	/**
-	 * Opens an SQLite connection using PDO_SQLITE
+	 * Opens an SQLite connection using PDO_SQLITE.
+	 *
+	 * Opens an SQLite connection using PDO_SQLITE.  Accepts a file
+	 * location as input or special SQLite locations, such as :memory:
+	 * for in-memory databases, that are compatible with PDO_SQLITE.
 	 *
 	 * @param string $sqlite_file_location Path to SQLite file
 	 *
-	 * @throws \Exception If cannot open/create
+	 * @throws \Exception If cannot open/create the database file
 	 */
 	public function connect(string $sqlite_file_location) : void
 	{
@@ -28,7 +34,7 @@ class SQLite extends Wrapper
 		catch(\PDOException $e)
 		{
 			$this->db_link = null;
-			throw new \Exception("Failed to open database: ".$e->getMessage());
+			throw new ConnectFailed('Failed to open/create database: '.$e->getMessage(), 0, $e);
 		}
 	}
 }
